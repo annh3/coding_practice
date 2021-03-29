1fef19830040459f7e056e5ffe50be276bc300cd
Ann He <annhe@Anns-MacBook-Pro.local>
2021-03-29 14:37:59 -0700

    putting the pieces together

--- alien_dictionary.py
+++ alien_dictionary.py
@@ -1,22 +1,56 @@
 class Solution:
     def index_diff(self, str1, str2):
         index = next((i for i, (char1, char2) in enumerate(zip(str1, str2)) if char1 != char2), None)
-        return index               
+        return index  
     
-    def bfs_toposort(self, start, graph) -> List[str]:
-        q = []
-        q.append(start)
-        topo_sort = []
+    def DFS(self, cur, g, visited, in_process):
+        if cur == None or cur in visited:
+            return False
+
+        in_process.add(cur)
+
+        flag = False
+        for child in g[cur]:
+            if child in in_process:
+                return True
+            if child not in visited:
+                flag = flag or self.DFS(child, g, visited, in_process)
+        in_process.remove(cur)
+        visited.add(cur)
+        return flag
+
+    def DFS_wrapper(self, cur, g):
         visited = set()
-        parent = {}
+        in_process = set()
+        res = False
+        for node in g.keys():
+            res = res or self.DFS(node, g, visited, in_process)
+        return res
+    
+    def topo_sort(self,g): # a dictionary adjacency list
+
+        # pre-processing step 1
+        incoming_count = {a: 0 for a in g.keys()}
+        for a in g.keys():
+            for b in g[a]:
+                incoming_count[b] += 1
+
+        q = []
+        for a in incoming_count.keys():
+            if incoming_count[a] == 0:
+                q.append(a)
+
+        sort = []
         while len(q) > 0:
             cur = q.pop(0)
-            topo_sort.append(cur)
-            for c in graph[cur]:
-                if c not in visited:
-                    visited.add(c)
-                    q.append(c)
-        return topo_sort
+            sort.append(cur)
+            # decrease
+            for child in g[cur]:
+                incoming_count[child] -= 1
+                if incoming_count[child] <= 0:
+                    q.append(child)
+
+        return sort
     
     def alienOrder(self, words: List[str]) -> str:
         alphabet = set(''.join(words)) # all unique characters
@@ -41,15 +75,23 @@ class Solution:
             index = self.index_diff(words[i], words[i-1])
             if index is not None:
                 adj[alphabet_map[words[i-1][index]]][alphabet_map[words[i][index]]] = 1
+            if index is None and len(words[i-1]) > len(words[i]):
+                return ""
         
         g_edges = {a: [] for a in alphabet}
         for i in range(len(alphabet)):
             for j in range(len(alphabet)):
                 if adj[i][j] == 1:
                     g_edges[alphabet[i]].append(alphabet[j])
+        print("g_edges: ", g_edges)
+        
+        # cycle detection
+        res = self.DFS_wrapper(words[0][0], g_edges)
+        if res == True:
+            return ""
         
         # now create an ordering
-        topo_sort = self.bfs_toposort(words[0][0], g_edges)
+        topo_sort = self.topo_sort(g_edges)    
         diff = set(alphabet) - set(topo_sort)
         topo_sort.extend(list(diff))
         
