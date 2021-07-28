3b9a5523552b5cbd28105b5a6fe63dbf2a0912ba
Ann He <annhe@Anns-MacBook-Pro.local>
2021-07-28 16:32:06 -0700

    update

--- prefix_suffix_search.py
+++ prefix_suffix_search.py
@@ -1,48 +1,59 @@
 class WordFilter:
 
     def __init__(self, words: List[str]):
-        self.prefixes_tree = {c: {} for c in string.ascii_lowercase}
-        
+        self.prefixes_tree = {c: {'idxs': [], 'tree':{}} for c in string.ascii_lowercase}
         for i,word in enumerate(words):
             word = word.lower()
-            cur = self.prefixes_tree
-            for l in word:
+            cur = self.prefixes_tree[word[0]]
+            for l in word[1:]:
                 if l in cur:
-                    cur = cur[l]
+                    cur['idxs'].append(i)
+                    cur = cur['tree'][l]
+                    
                 else:
-                    cur[l] = {}
-                    cur = cur[l]
-            cur['*'] = {i}
+                    cur[l] = {'idxs': [], 'tree':{}}
+                    cur[l]['idxs'].append(i)
+                    cur = cur[l]['tree']
+            #cur['*'] = {i}
         
         self.suffixes_tree = {c: {} for c in string.ascii_lowercase}
         
-        for i,word in words:
-            word = reversed(word).lower()
-            cur = self.suffixes_tree
-            for l in word:
+        for i,word in enumerate(words):
+            word = word[::-1]
+            word = word.lower()
+            cur = self.suffixes_tree[word[0]]
+            for l in word[1:]:
                 if l in cur:
-                    cur = cur[l]
+                    cur['idxs'].append(i)
+                    cur = cur['tree'][l]
+                    
                 else:
-                    cur[l] = {}
-                    cur = cur[l]
-            cur['*'] = {i}
+                    cur[l] = {'idxs': [], 'tree':{}}
+                    cur[l]['idxs'].append(i)
+                    cur = cur[l]['tree']
         
 
     def f(self, prefix: str, suffix: str) -> int:
         suffix_idx = -1
         prefix_idx = -1
-        
+        print("exists: ", len(self.prefixes_tree))
         # search prefix tree
         cur = self.prefixes_tree
         broke_out = False
-        
+        print("prefix: ", prefix)
         for l in prefix:
-            cur = cur[l]
-        else:
-            broke_out = True
-            
-        if not broke_out and '*' in cur:
-            prefix_idx = cur['*']
+            print("l: ", l)
+            if l in cur:
+                cur = cur[l]['tree']
+                #print("cur length: ", len(cur))
+            else:
+                print("breaking out")
+                broke_out = True
+        
+        print("cur at bottom: ", cur)
+        if not broke_out:
+            # traverse the tree
+            prefix_idx = max(cur['idxs'])
             
             
         # search suffix tree
@@ -50,12 +61,13 @@ class WordFilter:
         broke_out = False
         
         for l in suffix:
-            cur = cur[l]
-        else:
-            broke_out = True
+            if l in cur:
+                cur = cur['tree'][l]
+            else:
+                broke_out = True
             
-        if not broke_out and '*' in cur:
-            suffix_idx = cur['*']
+        if not broke_out:
+            suffix_idx = max(cur['idxs'])
             
         return max(prefix_idx, suffix_idx)
         
